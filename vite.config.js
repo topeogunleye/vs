cc94fad2e2ad2174026731f9163c829f10b7fdf5
“Temitope <topeogunleye@gmail.com>
2024-05-20 00:13:00 +0100

    Update vite configuration for tests

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    // this points to the setup file that we created earlier
+    setupFiles: "./src/setup.js",
+    // you might want to disable the `css: true` line, since we don't have
+    // tests that rely on CSS -- and parsing CSS is slow.
+    // I'm leaving it in here becasue often people want to parse CSS in tests.
+    css: true,
+  },
+});
